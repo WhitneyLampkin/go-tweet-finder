0e41f233879c1aa3104ad5630624b173d65d6404
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-05-25 13:25:23 -0500

    adding cat facts function

--- main.go
+++ main.go
@@ -8,9 +8,22 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting server...")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
-
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	getCatFacts()
+}
+
+func getCatFacts() {
+	response, error := http.Get("https://cat-fact.herokuapp.com/facts")
+	if error != nil {
+		fmt.Println(response)
+	}
 }
